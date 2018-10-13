7c89f6654e10fec15e14c5c07dfe025fa3ef5a89
Centanoul <centanoul@gmail.com>
2018-10-13 01:58:32 -0700

    Update alliance/allies.js

--- alliance/allies.js
+++ alliance/allies.js
@@ -1,5 +1,5 @@
 module.exports.isAlly = function(unknownObject) {
-	if (["Centanoul", "Isktrasow"].indexOf(unknownObject) >= 0){
+	if (["Centanoul", "Isktrasow"].indexOf(unknownObject.owner.username) >= 0){
 		return true;
 	} else { return false; }
 }
